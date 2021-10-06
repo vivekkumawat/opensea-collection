7cf88bbbdb60bf528984a2e8d33eafc30839eaad
vivekkumawat-refyne <vivek.kumawat@refyne.co.in>
2021-10-06 19:20:37 +0530

    updated script

--- app.js
+++ app.js
@@ -4,11 +4,17 @@ const cors = require("cors");
 const app = express();
 const mongoose = require("mongoose");
 const port = process.env.PORT || 5000;
-const statusCode = require("./src/utilities/statusCode");
+
+const collectionsHashes = require("./final_data_slug.json");
+const Status = require("./src/controllers/status");
+const logStatus = new Status();
+const Asset = require("./src/models/Asset");
+const Collection = require("./src/models/Collection");
 const {
-  fetchAndStoreAssets,
-  apiStatus,
-} = require("./src/controllers/FetchAndStoreAssets");
+  getAssetsByHash,
+  getCollectionDetailsByHash,
+} = require("./src/external-service/openSea");
+const statusCode = require("./src/utilities/statusCode");
 
 // Connect to MongoDB ATLAS
 mongoose.connect(
@@ -33,10 +39,6 @@ app.use(
   })
 );
 
-app.get("/start", (req, res) => {
-  fetchAndStoreAssets(req, res);
-});
-
 app.get("/status", (req, res) => {
   apiStatus(req, res);
 });
@@ -50,3 +52,74 @@ app.use("/", (req, res, next) => {
 app.listen(port, () => {
   console.log(`Server up and running on port ${port}`);
 });
+
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+const fetchAndStoreAssets = async () => {
+  try {
+    for (const collection of collectionsHashes.collection) {
+      const collectionHash = collection.contracts[0].contract;
+      const fetchForTokenId = await getAssetsByHash(
+        collectionHash,
+        "pk",
+        "asc",
+        0,
+        1
+      );
+      const collectionDetails = await getCollectionDetailsByHash(
+        collectionHash,
+        fetchForTokenId.assets[0].token_id
+      );
+
+      await Collection.create({
+        collection_hash: collectionHash,
+        collection_details: collectionDetails,
+      });
+
+      const noOfCalls = Math.ceil(
+        collectionDetails.collection.stats.total_supply / 50
+      );
+      let offset = 0;
+      for (let i = 1; i <= noOfCalls; i++) {
+        const asset = await getAssetsByHash(
+          collectionHash,
+          "pk",
+          "asc",
+          offset,
+          50
+        );
+        if (asset) {
+          for (const nft of asset.assets) {
+            Asset.create({
+              collection_hash: nft.asset_contract.address,
+              asset: nft,
+            });
+          }
+          logStatus.updateStatus("true", 0, i);
+          await sleep(5000);
+        }
+        offset += 50;
+      }
+    }
+  } catch (error) {
+    logStatus.updateStatus("false", 1, 0);
+    console.log(error);
+  }
+};
+
+fetchAndStoreAssets();
+
+const apiStatus = async (req, res) => {
+  const currentStatus = logStatus.getStatus();
+  return res.status(statusCode.success.code).json({
+    currentStatus,
+  });
+};
+
+module.exports = {
+  fetchAndStoreAssets,
+  apiStatus,
+};
