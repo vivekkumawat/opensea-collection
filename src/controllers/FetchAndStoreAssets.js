1d498dd627be2f3953e8b69dc8b29876595886ac
vivekkumawat-refyne <vivek.kumawat@refyne.co.in>
2021-10-06 18:00:03 +0530

    updated script

--- src/controllers/FetchAndStoreAssets.js
+++ src/controllers/FetchAndStoreAssets.js
@@ -8,7 +8,11 @@ const {
   getCollectionDetailsByHash,
 } = require("../external-service/openSea");
 const statusCode = require("../utilities/statusCode");
-
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
 const fetchAndStoreAssets = async (req, res) => {
   try {
     for (const collection of collectionsHashes.collection) {
@@ -34,7 +38,6 @@ const fetchAndStoreAssets = async (req, res) => {
         collectionDetails.collection.stats.total_supply / 50
       );
       let offset = 0;
-      let promises = [];
       for (let i = 1; i <= noOfCalls; i++) {
         const asset = await getAssetsByHash(
           collectionHash,
@@ -43,34 +46,19 @@ const fetchAndStoreAssets = async (req, res) => {
           offset,
           50
         );
-        promises.push(asset);
-        logStatus.updateStatus("false", 0, i);
+        if (asset) {
+          for (const nft of asset.assets) {
+            Asset.create({
+              collection_hash: nft.asset_contract.address,
+              asset: nft,
+            });
+          }
+          logStatus.updateStatus("true", 0, i);
+          await sleep(5000);
+        }
         offset += 50;
       }
     }
-
-    Promise.all(promises)
-      .then((response) => {
-        if (response) {
-          for (const res of response) {
-            for (const nft of res.assets) {
-              Asset.create({
-                collection_hash: nft.asset_contract.address,
-                asset: nft,
-              });
-            }
-          }
-          logStatus.updateStatus("true", 0, response.length);
-        }
-        return res.status(statusCode.success.code).json({
-          error: false,
-          assets: response,
-        });
-      })
-      .catch((error) => {
-        logStatus.updateStatus("false", 1, 0);
-        console.log(error);
-      });
   } catch (error) {
     logStatus.updateStatus("false", 1, 0);
     console.log(error);
